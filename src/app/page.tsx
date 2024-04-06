ca772384bb8bbafd8c4b3e2a283a2960990af666
Rose Francis <rosennabs@gmail.com>
2024-04-06 01:17:09 -0600

    Add a click event to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
-import "tailwindcss/tailwind.css";
+'use client'
+import 'tailwindcss/tailwind.css';
 
 function Home() {
+
+  const handleButtonClick = () => {
+  console.log("Button Clicked");
+  
+}
+
   return (
     <div
-      className="h-screen flex items-center justify-center"
+      className='h-screen flex items-center justify-center'
       style={{ backgroundImage: `url('/background.jpg')` }}
     >
-      <div className="flex flex-col items-center">
-        <h1 className="text-amber-600 text-5xl mb-16 uppercase"> Welcome to DrumFest! </h1>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-amber-600 text-5xl mb-16 uppercase'>
+          {' '}
+          Welcome to DrumFest!{' '}
+        </h1>
         <button
-          type="button"
-          id="startButton"
-          className="bg-green-700 hover:bg-green-800 text-white font-bold py-5 px-16 rounded-md text-3xl"
+          type='button'
+          onClick={()=> handleButtonClick()}
+          className='bg-green-700 hover:bg-green-800 text-white font-bold py-5 px-16 rounded-md text-3xl'
         >
           START
         </button>
