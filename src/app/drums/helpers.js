c589aa5930abd81523011d5ac254a128cee77269
Rose Francis <rosennabs@gmail.com>
2024-04-08 04:55:08 -0600

    Refactor

---
+++ src/app/drums/helpers.js
@@ -0,0 +1,38 @@
+export const drumHits = [
+  {
+    key: "A",
+    sound: "CLAP",
+  },
+  {
+    key: "S",
+    sound: "HIHAT",
+  },
+  {
+    key: "D",
+    sound: "KICK",
+  },
+  {
+    key: "F",
+    sound: "OPENHAT",
+  },
+  {
+    key: "G",
+    sound: "BOOM",
+  },
+  {
+    key: "H",
+    sound: "RIDE",
+  },
+  {
+    key: "J",
+    sound: "SNARE",
+  },
+  {
+    key: "K",
+    sound: "TOM",
+  },
+  {
+    key: "L",
+    sound: "TINK",
+  },
+];
--- src/app/drums/page.tsx
+++ src/app/drums/page.tsx
@@ -1,49 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
+import {drumHits} from './helpers';
 
 
 function Drums() {
-  const drumHits = [
-    {
-    key: "A",
-    sound: "CLAP"
-    },
-    {
-    key: "S",
-    sound: "HIHAT"
-    },
-    {
-    key: "D",
-    sound: "KICK"
-    },
-    {
-    key: "F",
-    sound: "OPENHAT"
-    },
-    {
-    key: "G",
-    sound: "BOOM"
-    },
-    {
-    key: "H",
-    sound: "RIDE"
-    },
-    {
-    key: "J",
-    sound: "SNARE"
-    },
-    {
-    key: "K",
-    sound: "TOM"
-    },
-    {
-    key: "L",
-    sound: "TINK"
-    },
-
-  ]; 
-
+  
   return (
     <div className='h-screen w-100 flex flex-col items-center justify-center'
       style={{ backgroundImage: `url('/gameBackground.jpg')` }}>
@@ -51,8 +13,6 @@ function Drums() {
         <h1 className='text-5xl text-white mb-16'>Hit the drums!</h1>
 
         
-
-    
         <div className='flex flex-row justify-evenly'>
           {drumHits.map((hit) => (
             <div key={hit.key} className='flex flex-col items-center border-solid border-4 border-green-400 mx-4 py-4 px-8 mb-16' style={{ width: '100px' }}>
