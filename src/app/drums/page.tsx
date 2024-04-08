9a377a69f90ec092caf67e903f315d919893a97d
Rose Francis <rosennabs@gmail.com>
2024-04-08 06:23:00 -0600

    Change boder color when the keyboard key is pressed

--- src/app/drums/page.tsx
+++ src/app/drums/page.tsx
@@ -1,35 +1,71 @@
-import React from 'react'
-import Link from 'next/link';
-import 'tailwindcss/tailwind.css';
-import {drumHits} from './helpers';
+"use client";
 
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import "tailwindcss/tailwind.css";
+import { drumHits } from "./helpers";
 
 function Drums() {
-  
-  return (
-    <div className='h-screen w-100 flex flex-col items-center justify-center'
-      style={{ backgroundImage: `url('/gameBackground.jpg')` }}>
-      <div className='w-90 flex flex-col items-center'>
-        <h1 className='text-5xl text-white mb-16'>Hit the drums!</h1>
+  const [borderColor, setBorderColor] = useState({});
 
-        
-        <div className='flex flex-row justify-evenly'>
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const hit = drumHits.find(
+        (hit) => event.key.toLowerCase() === hit.key.toLowerCase()
+      );
+
+      if (hit) {
+        setBorderColor((prev) => ({
+          ...prev,
+          [hit.key]: "border-amber-400",
+        }));
+      }
+    };
+
+    const handleKeyUp = () => {
+      setBorderColor("border-green-400");
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return (
+    <div
+      className="h-screen w-100 flex flex-col items-center justify-center"
+      style={{ backgroundImage: `url('/gameBackground.jpg')` }}
+    >
+      <div className="w-90 flex flex-col items-center">
+        <h1 className="text-5xl text-white mb-16">Hit the drums!</h1>
+
+        <div className="flex flex-row justify-evenly">
           {drumHits.map((hit) => (
-            <div key={hit.key} className='flex flex-col items-center border-solid border-4 border-green-400 mx-4 py-4 px-8 mb-16' style={{ width: '100px' }}>
-            <p className='text-4xl text-white'>{hit.key}</p>
-            <p className='text-amber-400 text-sm'>{hit.sound}</p>
-          </div>
-        ))}
-          
+            <div
+              key={hit.key}
+              className={`flex flex-col items-center border-solid border-4 ${
+                borderColor[hit.key] || "border-green-400"
+              } mx-4 py-4 px-8 mb-16`}
+              style={{ width: "100px" }}
+            >
+              <kbd className="text-4xl text-white">{hit.key}</kbd>
+              <p className="text-amber-400 text-sm">{hit.sound}</p>
+            </div>
+          ))}
         </div>
 
-        <Link href='/'>
-          <span className='text-2xl underline underline-offset-4 text-white'>Finish</span>
+        <Link href="/">
+          <span className="text-2xl underline underline-offset-4 text-white">
+            Finish
+          </span>
         </Link>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default Drums;
